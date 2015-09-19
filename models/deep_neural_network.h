72c5bb9e9a613b796f5b33d2c7d69e4f471b95a7
superjom <superjom@gmail.com>
2015-09-19 12:40:35 +0800

    add models

---
+++ models/deep_neural_network.h
@@ -0,0 +1,101 @@
+#pragma once
+#include "../layers/neuron_layer.h"
+#include "../layers/map_layer.h"
+#include "../layers/loss_layer.h"
+#include "../layers/neural_network_layer.h"
+
+template<typename T>
+class DeepNeuralNetworks {
+public:
+    typedef typename Layer<T>::param_t param_t;
+    typedef typename Layer<T>::vec_t vec_t;
+    /*
+     * @sizes: length of hidden vector, from bottom to top
+     */
+    void setup(vector<int> sizes) {
+        CHECK_GT(sizes[0], 1) << "input layer should have more than 1 element";
+        CHECK_EQ(sizes[ sizes.size()-1 ], 1) << "output layer should have only 1 element";
+        shape_t shape;
+        setup_input_layer(sizes[0]);
+        shape.size = sizes[ sizes.size() - 1];
+        shape.width = shape.size;
+        setup_output_layer(shape);
+        // setup sigmoid layers and neuron layers
+        layers.resize(sizes.size());
+        for (int i = 1; i < sizes.size(); i++) {
+            LOG(WARNING) << "-- setup " << i << "th layer";
+            layers[i].reset(new NeuronNetworkLayer<T>);
+            auto& layer = *layers[i];
+            layer.set_name(swift_snails::format_string("neuron-%d", i));
+            shape = {sizes[i], sizes[i-1]};
+            layer.setup(shape);
+            if (i > 1) {
+                layer.set_bottom_layer(layers[i-1].get());
+                layers[i-1]->set_top_layer(layers[i].get());
+            }
+        }
+        layers[1]->set_bottom_layer(&data_layer);
+        layers[layers.size()-1]->set_top_layer(&rmse_loss_layer);
+    }
+
+    float learn(vec_t& vec, T label) {
+        rmse_loss_layer.param().label[0] = label;
+        data_layer.forward(vec);
+        Layer<T>* layer = layers[0];
+        // forward
+        while(layer != nullptr) {
+            layer->forward(layer->bottom_layer().param());
+        }
+        if (layer->kind() == HIDDEN_LAYER) layer = layer->top_layer();
+        // backward
+        rmse_loss_layer.backward();
+        layer = layers[ layers.size() - 1];
+        while (layer != nullptr) {
+            if (layer->kind() != INPUT_LAYER) 
+            {
+                CHECK_NE(layer->top_layer(), nullptr);
+                CHECK_NE(layer->bottom_layer(), nullptr);
+                layer->backward(
+                    layer->top_layer()->param(), 
+                    layer->bottom_layer()->param());
+            }
+            if (layer->kind() == HIDDEN_LAYER) layer = layer->bottom_layer();
+        }
+        auto loss = rmse_loss_layer.z[0];
+        return loss;
+    }
+
+    T predict(vec_t& vec) {
+
+    }
+protected:
+    void setup_input_layer(int size) {
+        data_layer.set_name("data");
+        data_layer.setup(size);
+    }
+    /*
+     * set sigmoid as output function
+     */
+    void setup_output_layer(const shape_t& shape) {
+        rmse_loss_layer.set_name("RMSE");
+        rmse_loss_layer.setup(shape);
+    }
+    /*
+     * a neural-network-layer is a neuron-layer with a tanh-layer
+     */
+    void set_neural_network_layer(int id, const shape_t &shape_, Layer<T>& neuron_layer, Layer<T>& tanh_layer) {
+        shape_t shape = shape_;
+        neuron_layer.set_name("neuron-%d", id);
+        tanh_layer.set_name("tanh-%d", id);
+        neuron_layer.gaus_dist().init(0, 0.7);
+        neuron_layer.setup(shape);
+        shape.width = shape.size;
+        tanh_layer.setup(shape);
+    }
+
+private:
+    DataLayer<T> data_layer;
+    // neuron layers and sigmoid layers
+    vector<std::shared_ptr<Layer<T> > > layers;
+    RMSELayer<T> rmse_loss_layer;
+};
---
+++ models/logistic_regression.h
@@ -0,0 +1,62 @@
+#pragma once
+#include "../layers/neuron_layer.h"
+#include "../layers/map_layer.h"
+#include "../layers/loss_layer.h"
+
+template<typename T>
+class LogisticRegression {
+public:
+    typedef typename Layer<T>::param_t param_t;
+    typedef typename Layer<T>::vec_t vec_t;
+    /*
+     * @size: length of hidden vector
+     */
+    void setup(int size) {
+        vector<shape_t> shapes = { shape_t(1, size) };
+        neuron_layer.set_name("neural-layer");
+        neuron_layer.gaus_dist().init(0, 0.5);
+        neuron_layer.setup(shapes);
+        shapes[0].size = 1;
+        shapes[0].width = 1;
+
+        sigmoid_layer.set_name("sigmoid-layer");
+        sigmoid_layer.gaus_dist().init(0, 0.5);
+        sigmoid_layer.setup(shapes);
+
+        loss_layer.set_name("rmse-layer");
+        loss_layer.gaus_dist().init(0, 0.5);
+        loss_layer.setup(shapes);
+
+        data_layer.set_name("data-layer");
+        data_layer.setup(size);
+    }
+
+    float learn(vec_t& vec, T label) {
+        loss_layer.param().label[0] = label;
+        data_layer.forward(vec);
+        neuron_layer.forward(data_layer.param());
+        sigmoid_layer.forward(neuron_layer.param());
+        loss_layer.forward(sigmoid_layer.param());
+        float loss = loss_layer.param().z[0];
+        // backward
+        loss_layer.backward(sigmoid_layer.param());
+        sigmoid_layer.backward(loss_layer.param(), neuron_layer.param());
+        neuron_layer.backward(sigmoid_layer.param(), data_layer.param());
+        return loss;
+    }
+
+    T predict(vec_t& vec) {
+        data_layer.forward(vec);
+        neuron_layer.forward(data_layer.param());
+        sigmoid_layer.forward(neuron_layer.param());
+        loss_layer.forward(sigmoid_layer.param());
+        return sigmoid_layer.param().z[0];
+    }
+
+private:
+    NeuronLayer<T> neuron_layer;
+    SigmoidLayer<T> sigmoid_layer;
+    RMSELayer<T> rmse_loss_layer;
+    DataLayer<T> data_layer;
+    RMSELayer<T> loss_layer;
+};
